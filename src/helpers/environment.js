fec9b2f6263cbcbce492f660d29f1d8ba23836ce
Nick Birch <nick@birchstartups.com>
2020-10-13 16:06:44 -0400

    update for deployment

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,12 +4,12 @@ switch (window.location.hostname) {
     case "localhost" || "127.0.0.1":
         API_URL = `http://localhost:${process.env.REACT_APP_SERVER_PORT}`;
         break;
-    // case "tw-blue-badge-server":
-    //     API_URL = "https://tw-blue-badge-server.herokuapp.com";
-    //     break;
-    // case "tw-gaming-application.herokuapp.com":
-    //     API_URL = "https://tw-blue-badge-server.herokuapp.com";
-    //     break;
+    case "red-badge-skill-app-server":
+        API_URL = "https://red-badge-skill-app-server.herokuapp.com";
+        break;
+    case "red-badge-skill-app.herokuapp.com":
+        API_URL = "https://red-badge-skill-app-server.herokuapp.com";
+        break;
     default:
         API_URL = `http://localhost:${process.env.REACT_APP_SERVER_PORT}`;
 };
