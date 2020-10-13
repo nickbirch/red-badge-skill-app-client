0e2706ac8a33ad26b8364fc006c2beeef4084264
Nick Birch <nick@birchstartups.com>
2020-10-13 16:21:00 -0400

    fixed API url

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,11 +4,11 @@ switch (window.location.hostname) {
     case "localhost" || "127.0.0.1":
         API_URL = `http://localhost:${process.env.REACT_APP_SERVER_PORT}`;
         break;
-    case "red-badge-skill-app-server":
-        API_URL = "https://red-badge-skill-app-server.herokuapp.com";
+    case "red-badge-skills-app-server":
+        API_URL = "https://red-badge-skills-app-server.herokuapp.com";
         break;
     case "red-badge-skill-app.herokuapp.com":
-        API_URL = "https://red-badge-skill-app-server.herokuapp.com";
+        API_URL = "https://red-badge-skills-app-server.herokuapp.com";
         break;
     default:
         API_URL = `http://localhost:${process.env.REACT_APP_SERVER_PORT}`;
